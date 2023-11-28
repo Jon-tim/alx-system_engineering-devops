74ad531181544b72a56fd29ffcb29ff5961e46d3
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-28 15:26:56 -0800

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,16 +1,19 @@
 # Install Nginx web server (w/ Puppet)
 
-# install NGINX
+# Filename: nginx_config.pp
 
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
+# Ensure Nginx service is running
 service { 'nginx':
   ensure => running,
-  enable => true
+  enable => true,
 }
 
+# Configure Nginx to listen on port 80
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
   content => '
@@ -18,18 +21,31 @@ server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
     server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
 
     location = /redirect_me {
         return 301 https://google.com;
+    }
+
+    location = / {
+        return 200 "Hello World!\n";
     }
 }
 ',
   notify  => Service['nginx'],
 }
 
+# Enable the default Nginx site
 file { '/etc/nginx/sites-enabled/default':
   ensure => link,
   target => '/etc/nginx/sites-available/default',
   notify => Service['nginx'],
 }
+
