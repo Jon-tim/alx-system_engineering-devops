7d94a6bfa29793a933f549b6ddaaf91a877528a7
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-01-02 14:54:12 -0800

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a Python script that export data in the CSV format.
+"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    ID = sys.argv[1]
+
+    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{ID}/todos')
+    USER = requests.get(f'https://jsonplaceholder.typicode.com/users/{ID}/'
+                        )
+    USER_ID = USER.json().get("id")
+    USERNAME = USER.json().get("username")
+
+    data = [
+        [USER_ID, USERNAME, item.get("completed"), item.get("title")]
+        for item in r.json()
+    ]
+
+    with open("USER_ID.csv", 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL, quotechar='"')
+        writer.writerows(data)
