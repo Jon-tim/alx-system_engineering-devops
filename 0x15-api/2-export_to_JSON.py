b383f416e357e7adbc8af5ea6b399f4933ca6506
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-01-02 15:37:06 -0800

    export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a Python script that export data in the JSON format.
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    ID = sys.argv[1]
+
+    r = requests.get(f'https://jsonplaceholder.typicode.com/users/{ID}/todos')
+    USER = requests.get(f'https://jsonplaceholder.typicode.com/users/{ID}/')
+    USER_ID = USER.json().get("id")
+    USERNAME = USER.json().get("username")
+
+    dictionary = {USER_ID: [{"task": item.get("title"),
+                             "completed": item.get("completed"),
+                             "username": USERNAME} for item in r.json()
+                            ]}
+    with open("USER_ID.json", "w", encoding="utf-8") as file:
+        json.dump(dictionary, file)
